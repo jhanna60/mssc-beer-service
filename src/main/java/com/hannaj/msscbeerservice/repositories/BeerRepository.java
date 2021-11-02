a3a13f5a0981483636afcae6306fbdae417c63e0
john hanna <jhanna60@hotmail.com>
2021-11-02 22:17:59 +0000

    Adding beer repository, closes #6

---
+++ src/main/java/com/hannaj/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.hannaj.msscbeerservice.repositories;
+
+import com.hannaj.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
